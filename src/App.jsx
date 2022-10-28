c392a80823aaeee1f6d6bd9db39f9adaeed2ffbb
Rey Abdul <reyabdul@gmail.com>
2022-10-28 13:57:58 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+
+    <div className="main-page">
+      <div className='main-overlay'>
+
+        <div className='header'>
+          <p id='aca-header'>ACA</p>
+        </div>
+
+        <div id='site-details'>
+          <p>A collection dedicated to the muse of Asian Cinema, archived through essays, video and design.</p>
+        </div>
+
+        <div className='links'>
+
+        </div>
+
+      </div>
+    </div>
+
+  )
+
+};
+
+export default App;
